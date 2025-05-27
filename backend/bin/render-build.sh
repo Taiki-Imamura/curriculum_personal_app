dbbf15f40773b6d6d8e48e045efd859150e16a8b
Taiki Imamura <taiki.imamura@play.jp>
2025-05-27 12:38:58 +0900

    fix: render-buildを元に戻した

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-
-# Exit on error
+# exit on error
 set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
-
-# If you have a paid instance type, we recommend moving
-# database migrations like this one from the build command
-# to the pre-deploy command:
-bin/rails db:migrate
+bundle exec rake db:migrate
